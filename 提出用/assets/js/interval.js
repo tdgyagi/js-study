be65b3ac117531e593b569136cee6bf87980bcbe
iida <iida@iida-no-MacBook-Pro.local>
2016-10-07 18:04:53 +0900

    [add] 提出用ファイルを追加

---
+++ 提出用/assets/js/interval.js
@@ -0,0 +1,19 @@
+/*js file*/
+$(function(){
+
+  var time = 800;
+
+  var w = $(window).width();
+  var h = $(window).height();
+
+  var ID = setInterval(function(){move()}, time);
+
+  function move(){
+        $(".square").animate({
+            'top': Math.floor( Math.random()*w ),
+            'left': Math.floor( Math.random()*h )
+        },400);
+  }
+
+
+})//end function
