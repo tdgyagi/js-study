f9e05c18b85eeee1d8177c3b6070c32fa4cb5aed
Koutarou Yagi <koutarou_yagi@udstudio.co.jp>
2016-10-09 00:34:35 +0900

    [cahnge] forサンプルを更新

--- study5/for/assets/js/for1.js
+++ study5/for/assets/js/for1.js
@@ -1,23 +1,38 @@
 /*js file*/
 $(function(){
 
-
    $("body").append("<div class='container'></div>");
    var $container = $(".container");
 
    var items = 100;
    var xLine = 10;
    var yLine = 10;
+   var scale = 70;
 
-   for(var i=0; i<=xLine; i++){
-     $container.append("<div class='box box"+i+"'></div");
+   for(var i=0; i<xLine; i++){
+     for(var n=0; n<yLine; n++){
+
+       $container.append("<div class='box box"+i+"_"+n+"'></div");
+       var $box =$(".box"+i+"_"+n)
+       $box.css({
+         "width": scale,
+         "height": scale,
+         "top": i * scale,
+         "left": n * scale,
+         "background-color":"#ccc",
+         "position": "absolute",
+         "border": "solid 1px #fefefe",
+         "text-align": "center",
+         "font-size": "12px",
+         "line-height": scale+"px"
+        });
+
+       $box.text((i+1)+"_"+(n+1));
+
+     }
    }
 
-   $(".box").css({
-     "width":"50px",
-     "height":"50px",
-     "background-color":"#ccc"
-   });
+
 
 
 
