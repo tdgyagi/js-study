7f223ce181b890eafb91e3e216d8c6f31762b69e
Koutarou Yagi <koutarou_yagi@udstudio.co.jp>
2016-10-11 10:58:00 +0900

    [fix] サンプルを変更

--- study6/window/assets/js/window5.js
+++ study6/window/assets/js/window5.js
@@ -3,12 +3,14 @@ $(function(){
 
   var timer = false;
   $(window).resize(function() {
-    if (timer !== false) {
+    if (timer != false) {
       clearTimeout(timer);
     }
-    timer = setTimeout(function() {
-      console.log('リサイズされました');
-    }, 600);
+    timer = setTimeout(resizeEvent, 600);
   });
+
+  function resizeEvent(){
+    console.log('リサイズされました');
+  }
 
 })//end function
